62c1c8c4e4a9dd234c29465863f3ceb32aa43fbe
slo <slo@ciklum.com>
2016-11-29 18:06:05 +0200

    Magento 2.1 - removed old files

--- dev/tests/integration/testsuite/Magento/Bundle/_files/PriceCalculator/dynamic_bundle_product_with_catalog_rule.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Copyright © 2016 Magento. All rights reserved.
- * See COPYING.txt for license details.
- */
-
-require __DIR__ . '/dynamic_bundle_product.php';
-require __DIR__ . '/../../../CatalogRule/_files/catalog_rule_10_off_not_logged.php';
--- dev/tests/integration/testsuite/Magento/CatalogImportExport/_files/product_export_with_categories.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Copyright © 2016 Magento. All rights reserved.
- * See COPYING.txt for license details.
- */
-
-\Magento\TestFramework\Helper\Bootstrap::getInstance()->reinitialize();
-
-require dirname(dirname(__DIR__)) . '/Catalog/_files/categories.php';
